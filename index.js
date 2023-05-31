c202c40cfb89199c972dc28c5e0f1993f5f1fc76
Yasmin Hernandes <yasminhernandesjobs@gmail.com>
2023-05-31 11:49:28 -0300

    feat: add headers config

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const server = express()
 
 const plants = require('./src/data/plants.json')
 
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.get('/', (req, res) => {
   return res.json(plants)
 })
